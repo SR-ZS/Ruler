c45c88518bcb55431202e4e08e009f3de0f5807d
SR-ZS <shikimac42@gmail.com>
2026-02-26 18:27:44 +0800

    Initial Commit

---
+++ Ruler/RulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RulerApp.swift
+//  Ruler
+//
+//  Created by zhangshuai on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct RulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
